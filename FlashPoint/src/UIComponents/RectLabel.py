9fadb7fa9855355562dd0061c580baacc242380d
hawschiat <haw1997@hotmail.com>
2018-10-31 01:39:31 -0400

    fix getter issues

--- FlashPoint/src/UIComponents/RectLabel.py
+++ FlashPoint/src/UIComponents/RectLabel.py
@@ -35,7 +35,7 @@ class RectLabel(pygame.sprite.Sprite):
         self.image.fill(self.color)
         if self.txtobj:
             self.txtobj.set_center((self.rect.width/2), (self.rect.height/2))
-            self.image.blit(self.txtobj.get_surf(), self.txtobj.get_rect())
+            self.image.blit(self.txtobj.text_surf, self.txtobj.text_rect)
 
     def change_color(self, color: Tuple[int, int, int]):
         self.color = color
