e777f20a8a0c73169242e7cf988cb01bad0c2d11
Francis Piche <francisplanning@gmail.com>
2018-11-24 13:19:38 -0500

    Temp

--- FlashPoint/src/game_elements/game_board/Tile.py
+++ FlashPoint/src/game_elements/game_board/Tile.py
@@ -1,30 +1,32 @@
 import pygame
 
-from src.constants.Color import Color
+import src.constants.Color as Color
+
 
 class Tile(pygame.sprite.Sprite):
 
-    def __init__(self, screen: pygame.Surface, size: int=64):
+    def __init__(self, screen: pygame.Surface, x: int, y: int, size: int=64):
         """Create a tile, not sure what the sprite size should be..."""
         super().__init__()
         self.screen = screen
         self.image = pygame.Surface([size, size])
         self.rect = self.image.get_rect()
-        self.rect.move_ip(600, 100)
+        self.rect.move_ip(x, y)
         self.is_hovered = False
-        self.x_coordinate = 0
-        self.y_coordinate = 0
         self._render()
 
     def _render(self):
         """Eventually this might have some randomization logic? Dunno how we'll generate boards :( """
         self.image.fill(Color.GREY, self.rect)  # eventually this will be an actual tile image.
-        return ""
 
     def check_mouse_over(self):
         mouse = pygame.mouse.get_pos()
         rect = self.rect
-        return rect.x+rect.w > mouse[0] > rect.x and rect.y+rect.h > mouse[1] > rect.y
+        x_max = rect.x + rect.w
+        x_min = rect.x
+        y_max = rect.y + rect.h
+        y_min = rect.y
+        return x_max > mouse[0] > x_min and y_max > mouse[1] > y_min
 
     def update(self):
         if self.check_mouse_over():
