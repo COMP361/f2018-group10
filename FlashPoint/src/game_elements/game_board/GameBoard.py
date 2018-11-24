cf72b50438e8ea900ff68f9e9a00cc845ccb9f92
Francis Piche <francisplanning@gmail.com>
2018-11-24 15:45:52 -0500

    Fixed game board kinda

--- FlashPoint/src/game_elements/game_board/GameBoard.py
+++ FlashPoint/src/game_elements/game_board/GameBoard.py
@@ -4,43 +4,16 @@ import src.constants.MainConstants as MainConst
 from src.game_elements.game_board.Grid import Grid
 
 
-class GameBoard(object):
+class GameBoard(pygame.sprite.Group):
     """Wrapper class for the Grid class. Contains methods specific for user interfacing."""
-    def __init__(self,
-                 width: int=MainConst.SCREEN_RESOLUTION[0]*2/3,
-                 height: int=MainConst.SCREEN_RESOLUTION[1]*0.8):
-        self.image = pygame.Surface((width, height))
-        self.grid = Grid(screen=self.image)
-        self.rect = self.image.get_rect().move(
-            (MainConst.SCREEN_RESOLUTION[0] - width, MainConst.SCREEN_RESOLUTION[1] - height))
+    def __init__(self):
+        super().__init__()
+        self.image = pygame.Surface((MainConst.SCREEN_RESOLUTION[0]*2/3, MainConst.SCREEN_RESOLUTION[1]*0.8))
+        self.rect = self.image.get_rect().move((500, 100))
 
         self.is_scrolling = -1                              # Need 2 clicks to become 1 (True)
         self.mouse_pos = (0, 0)                             # Store the mouse pos to determine direction
+        self._init_sprites()
 
-    # def _scroll(self):
-    #     current_mouse_pos = pygame.mouse.get_pos()
-    #     pressed = pygame.mouse.get_pressed()[0]             # left click
-    #     movement = (current_mouse_pos[0] - self.mouse_pos[0], current_mouse_pos[1] - self.mouse_pos[1])
-    #
-    #     if pressed:
-    #         self.is_scrolling += 1
-    #         if self.is_scrolling >= 1:
-    #             # set motion direction
-    #             print(f"scrolling: {movement[0]} {movement[1]}")
-    #
-    #             # Move all the sprites:
-    #             for sprite in self.grid.sprites():
-    #                 if (self.rect.left < self.grid.grid[0][0].rect.left < self.rect.right
-    #                         and self.rect.bottom < self.grid.grid[0][0].rect.top < self.rect.top):
-    #                     sprite.rect.move_ip(movement)
-    #     else:
-    #         self.is_scrolling = -1
-    #
-    #     self.mouse_pos = current_mouse_pos
-
-    def draw(self, screen: pygame.display):
-        screen.blit(self.image, self.rect)
-
-    def update(self):
-        # self._scroll()
-        self.grid.update()
+    def _init_sprites(self):
+        self.add(Grid(x_coord=self.rect.left, y_coord=self.rect.top, screen=self.image))
--- FlashPoint/src/game_elements/game_board/Grid.py
+++ FlashPoint/src/game_elements/game_board/Grid.py
@@ -7,29 +7,30 @@ from src.game_elements.game_board.Tile import Tile
 
 class Grid(pygame.sprite.Group):
     """Class to Group Tile objects together, and implement Grid logic in to what will form the GameBoard."""
-    def __init__(self, *sprites: pygame.sprite.Sprite, screen: pygame.Surface=None, tile_size: int=64, tiles_x: int=12,
-                 tiles_y: int=8):
+    def __init__(self, *sprites: pygame.sprite.Sprite, screen: pygame.Surface=None,
+                 x_coord: int, y_coord: int,
+                 tile_size: int=64, tiles_x: int=12, tiles_y: int=8,):
         super().__init__(*sprites)
+        self.screen = screen
         self.height = tiles_y
         self.width = tiles_x
-        self.screen = screen
-        self.rect = pygame.Rect(screen.get_rect().left, screen.get_rect().top, tile_size*tiles_x, tile_size*tiles_y)
+        self.image = pygame.Surface((tile_size*tiles_x, tile_size*tiles_y))
+        self.rect = self.image.get_rect().move((x_coord, y_coord))
         self.grid = self._generate_grid()
 
     def _generate_grid(self, tile_size: int=64) -> List[List[Tile]]:
-        """Initialize an 8x6 grid of Tiles, add to self Sprite Group."""
+        """Initialize an grid of Tiles, add to self Sprite Group."""
         grid = []
         x_coord = 0
         for i in range(0, self.width):
             grid.append([])
             y_coord = 0
             for j in range(0, self.height):
-                grid[i].append(Tile(self.screen, self.rect.x + x_coord, self.rect.y + y_coord))
+                grid[i].append(Tile(self.rect.x + x_coord, self.rect.y + y_coord))
                 grid[i][j].x_coordinate = i
                 grid[i][j].y_coordinate = j
                 self.add(grid[i][j])
 
                 y_coord += tile_size
-
             x_coord += tile_size
         return grid
