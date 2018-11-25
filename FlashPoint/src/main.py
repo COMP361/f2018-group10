6634c4f845e864c36e62949632c1cfe76b0d6ad1
Francis Piche <francisplanning@gmail.com>
2018-11-25 14:57:23 -0500

    moar fixes

--- FlashPoint/src/main.py
+++ FlashPoint/src/main.py
@@ -4,17 +4,9 @@ import sys
 import pygame
 
 import src.constants.Color as Color
-from src.HostJoinScene import HostJoinScene
-from src.HostMenuScene import HostMenuScene
-from src.JoinScene import JoinScene
-from src.StartScene import StartScene
-from src.Windows.CharacterSelectionMenu.CharacterScene import CharacterSelectionMenu
-from src.Game_Intial_Menu import GameIntialMenu
-import src.constants.Color as Color
 import src.constants.MainConstants as MainConst
-from src.scenes.GameBoardScene import GameBoardScene
-
 from src.Windows.UIComponents.SceneManager import SceneManager
+from src.core.EventQueue import EventQueue
 
 
 class Main(object):
@@ -25,51 +17,33 @@ class Main(object):
         pygame.display.set_caption(MainConst.WINDOW_TITLE)
         self.screen = pygame.display.set_mode(MainConst.SCREEN_RESOLUTION)
         self.clock = pygame.time.Clock()
-        self._init_buttons()
+        self.scene_manager = SceneManager(self.screen)
+        self.event_queue = EventQueue()
 
         # this is what will help us switch from one scene to another.
 
         # each of these comments below are each of the scenes
 
     def main(self):
-        # Initialize pygame modules, get the screen and clock
-
         # Run main loop
         while True:
             # Lock frame rate at 60 FPS. Should only be called once per loop.
             self.clock.tick(60)
-
+            self.event_queue.fill_queue()
             self.screen.fill(Color.BLACK)
 
-            for event in pygame.event.get():
+            for event in self.event_queue:
                 if event.type == pygame.QUIT:
                     sys.exit()
 
             # Clear the screen to black and flip the double buffer
 
-            self.manager.draw()
-            self.manager.update()
+            self.scene_manager.draw()
+            self.scene_manager.update(self.event_queue)
+
+            self.event_queue.flush_queue()
 
             pygame.display.flip()
-
-    def _init_buttons(self):
-        self.hjs = HostJoinScene(self.screen)
-        self.hms = HostMenuScene(self.screen)
-        self.js = JoinScene(self.screen)
-        self.ss = StartScene(self.screen)
-        self.css = CharacterSelectionMenu(self.screen)
-        self.gim = GameIntialMenu(self.screen)
-        self.manager = SceneManager(self.screen, self.ss)
-        self.ss.buttonLogin.on_click(self.manager.switch, self.hjs)
-        self.ss.buttonRegister.on_click(self.manager.switch, self.hjs)
-        self.hjs.buttonJoin.on_click(self.manager.switch, self.js)
-        self.hjs.buttonHost.on_click(self.manager.switch, self.hms)
-        self.hjs.buttonBack.on_click(self.manager.switch, self.ss)
-        self.hms.buttonBack.on_click(self.manager.switch, self.hjs)
-        self.js.buttonBack.on_click(self.manager.switch, self.hjs)
-        self.hms.button1.on_click(self.manager.switch, self.gim)
-        self.gim.buttonBack.on_click(self.manager.switch,self.hms)
-        #self.gim.buttonRegister.on_click(self.manager.switch, self.)
 
 
 if __name__ == '__main__':
