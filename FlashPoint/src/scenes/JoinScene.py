9df788fa6ccb453ef3907bf6f073b3e15518d8a6
Nuri <nuri.amiraslan@mail.mcgill.ca>
2018-11-29 20:09:45 -0500

    Made sure Text Bar works

--- FlashPoint/src/scenes/JoinScene.py
+++ FlashPoint/src/scenes/JoinScene.py
@@ -8,14 +8,17 @@ from src.Windows.UIComponents.Scene import Scene
 from src.Windows.UIComponents.InputBox import InputBox
 
 
-class JoinScene(Scene):
+class JoinScene(object):
     def __init__(self, screen):
-        Scene.__init__(self, screen)
+
+        self.resolution = (1280, 700)
+        self.sprite_grp = pygame.sprite.Group()
         self._init_background()
         self._init_text_box(342, 350, "Enter IP:", Color.STANDARDBTN, Color.BLACK)
         self._init_text_bar(500, 350, 400, 32)
         self._init_btn(575, 536, "Connect", Color.STANDARDBTN, Color.BLACK)
         self._init_btn_back(20, 20, "Back", Color.STANDARDBTN, Color.BLACK)
+        self._text_bar = self._init_text_bar(500, 350, 400, 32)
 
     def _init_text_box(self, x_pos, y_pos, text, color: Color, color_text: Color):
         box_size = (136, 32)
@@ -37,11 +40,19 @@ class JoinScene(Scene):
         self.sprite_grp.add(self.buttonConnect)
 
     def _init_text_bar(self, x_pos, y_pos, width, height):
-        input_box1 = InputBox(x=x_pos, y=y_pos, w=width, h=height)
-        self.sprite_grp.add(input_box1)
+        return InputBox(x=x_pos, y=y_pos, w=width, h=height)
 
     def _init_btn_back(self, x_pos: int, y_pos: int, text: str, color: Color, color_text: Color):
         box_size = (130, 48)
         self.buttonBack = RectButton(x_pos, y_pos, box_size[0], box_size[1], color, 0,
                                      Text(pygame.font.SysFont('Arial', 20), text, color_text))
         self.sprite_grp.add(self.buttonBack)
+
+    def draw(self, screen):
+        self.sprite_grp.draw(screen)
+        self._text_bar.draw(screen)
+
+
+    def update(self, event_queue):
+        self.sprite_grp.update(event_queue)
+        self._text_bar.update(event_queue)
--- FlashPoint/src/scenes/StartScene.py
+++ FlashPoint/src/scenes/StartScene.py
@@ -9,16 +9,20 @@ from src.Windows.UIComponents.Scene import Scene
 from src.Windows.UIComponents.InputBox import InputBox
 
 
-class StartScene(Scene):
+class StartScene(object):
         def __init__(self, screen):
-            Scene.__init__(self, screen)
+            # Scene.__init__(self, screen)
+            self.resolution = (1280, 700)
+            self.sprite_grp = pygame.sprite.Group()
             self._init_background()
-            self._init_text_box(342, 350, "Username:", Color.STANDARDBTN,Color.BLACK)
-            self._init_text_box(342, 434, "Password:", Color.STANDARDBTN,Color.BLACK)
-            self._init_text_bar(500, 350, 400, 32)
-            self._init_text_bar(500, 434, 400, 32)
-            self._init_btn_login(594, 536, "Login", Color.STANDARDBTN,Color.BLACK)
-            self._init_btn_register(791, 536, "Register", Color.STANDARDBTN,Color.BLACK)
+
+            self._init_text_box(342, 350, "Username:", Color.STANDARDBTN, Color.BLACK)
+            self._init_text_box(342, 434, "Password:", Color.STANDARDBTN, Color.BLACK)
+            self._init_btn_login(594, 536, "Login", Color.STANDARDBTN, Color.BLACK)
+            self._init_btn_register(791, 536, "Register", Color.STANDARDBTN, Color.BLACK)
+
+            self._text_bar1 = self._init_text_bar(500, 350, 400, 32)
+            self._text_bar2 =  self._init_text_bar(500, 434, 400, 32)
 
         def _init_background(self):
             box_size = (self.resolution[0], self.resolution[1])
@@ -36,17 +40,17 @@ class StartScene(Scene):
             box_size = (136, 32)
 
             user_box = RectLabel(x_pos, y_pos, box_size[0], box_size[1], color, 0,
-                                 Text(pygame.font.SysFont('Arial', 20), text,color_text ))
+                                 Text(pygame.font.SysFont('Arial', 20), text, color_text ))
             self.sprite_grp.add(user_box)
 
-        def _init_btn_login(self, x_pos, y_pos, text,color,color_text):
+        def _init_btn_login(self, x_pos, y_pos, text, color, color_text):
             box_size = (130, 48)
             self.buttonLogin = RectButton(x_pos, y_pos, box_size[0], box_size[1], color, 0,
                                           Text(pygame.font.SysFont('Arial', 20), text, color_text))
 
             self.sprite_grp.add(self.buttonLogin)
 
-        def _init_btn_register(self, x_pos, y_pos, text,color,color_text):
+        def _init_btn_register(self, x_pos, y_pos, text, color, color_text):
             box_size = (130, 48)
             self.buttonRegister = RectButton(x_pos, y_pos, box_size[0], box_size[1], color, 0,
                                              Text(pygame.font.SysFont('Arial', 20), text, color_text))
@@ -54,5 +58,15 @@ class StartScene(Scene):
             self.sprite_grp.add(self.buttonRegister)
 
         def _init_text_bar(self, x_pos, y_pos, width, height):
-            input_box1 = InputBox(x=x_pos, y=y_pos, w=width, h=height)
-            self.sprite_grp.add(input_box1)
+            return InputBox(x=x_pos, y=y_pos, w=width, h=height)
+
+
+        def draw(self, screen):
+            self.sprite_grp.draw(screen)
+            self._text_bar2.draw(screen)
+            self._text_bar1.draw(screen)
+
+        def update(self, event_queue):
+            self.sprite_grp.update(event_queue)
+            self._text_bar1.update(event_queue)
+            self._text_bar2.update(event_queue)
