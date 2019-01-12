94b8da6744aa0e29b267f5323b9414c2782c1e5b
Seng Chiat Haw <haw1997@hotmail.com>
2019-01-12 12:20:54 -0500

    Update Networking.py

--- FlashPoint/src/core/Networking.py
+++ FlashPoint/src/core/Networking.py
@@ -1,4 +1,3 @@
-import sys
 import socket
 
 from src.external.Mastermind import *
@@ -10,7 +9,7 @@ class Networking(object):
     TIMEOUT_CONNECT = 200
     TIMEOUT_RECEIVE = 200
 
-    def create_host(self):
+    def create_host(self, port=20298):
         # We use UDP to broadcast the host
         self.host = MastermindServerUDP()
 
@@ -19,23 +18,35 @@ class Networking(object):
         for i in range(255, 0, -1):
             for j in range(255, 0, -1):
                 try:
+                    # try to connect to the specified ip address
                     socket.gethostbyaddr("192.168."+str(i)+"."+str(j))
                 except socket.error:
+                    # if we cannot connect to the address, that means the ip address is unused
                     server_ip = "192.168."+str(i)+"."+str(j)
                     break
             if server_ip is not None:
                 break
 
         try:
-            self.host.connect(server_ip, 20298)
+            self.host.connect(server_ip, port)
             # Start accepting connection
             self.host.accepting_allow_wait_forever()
+            # The host also acts as a client
+            self.client = MastermindClientUDP()
+            self.client.connect(server_ip, port)
         except MastermindError:
-                print("Server error!")
+            print("Server error!")
 
-    def join_host(self, ip, port):
+    def join_host(self, ip, port=20298):
         self.client = MastermindClientUDP()
         try:
             self.client.connect(ip, port)
         except MastermindError:
             print("Error connecting to server at: "+ip+":"+port)
+
+    @property
+    def is_host(self):
+        return self.host is not None
+
+    def send(self, data, compress=0):
+        self.client.send(data, compress)
