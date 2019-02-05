c961f4c5852abbf40598a17d69fab5b0c5656718
Seng Chiat Haw <haw1997@hotmail.com>
2019-02-05 18:27:26 -0500

    Revert "TRANSPARENCY WORKS"
    This reverts commit 01bb82c5dfb87197ccac1c3c6d524360301de219.

--- FlashPoint/src/UIComponents/ellipse_label.py
+++ FlashPoint/src/UIComponents/ellipse_label.py
@@ -51,8 +51,7 @@ class EllipseLabel(pygame.sprite.Sprite, Components):
         if isinstance(self.background, Tuple):
             pygame.draw.ellipse(self.image, self.background, self.rect, self.outer_width)
         else:
-            self.image = self.image.convert_alpha(pygame.display.get_surface())
-            self.image.fill((0, 0, 0, 0), None, pygame.BLEND_RGBA_MULT)
+            pygame.draw.ellipse(self.image, (0, 0, 0), self.rect, self.outer_width)
             image_file = FileImporter.import_image(self.background)
             self.image.blit(image_file, self.image)
 
