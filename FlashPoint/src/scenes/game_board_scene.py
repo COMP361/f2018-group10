3edf5d12ddaa02044239ef202237790589219136
Seng Chiat Haw <haw1997@hotmail.com>
2019-03-06 16:48:59 -0500

    minor bug fix

--- FlashPoint/src/scenes/game_board_scene.py
+++ FlashPoint/src/scenes/game_board_scene.py
@@ -29,8 +29,9 @@ class GameBoardScene(object):
     """Scene for displaying the main game view"""
 
     def __init__(self, screen: pygame.display, current_player: PlayerModel):
-        """:param screen : The display passed from main on which to draw the Scene."""
-
+        """
+        :param screen : The display passed from main on which to draw the Scene.
+        """
         self._save_games_file = "media/save_games.json"
         self.screen = screen
         self._game = GameStateModel.instance()
