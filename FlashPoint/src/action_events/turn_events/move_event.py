9b0428371e390cf939735001fe844282145a3682
Alek <alek.bedard@mail.mcgill.ca>
2019-03-06 11:26:04 -0500

    move_controller and choose_starting_position_controller in new folder

--- FlashPoint/src/action_events/turn_events/move_event.py
+++ FlashPoint/src/action_events/turn_events/move_event.py
@@ -5,22 +5,13 @@ from src.models.game_board.tile_model import TileModel
 from src.models.game_state_model import GameStateModel
 
 
-class Move(ActionEvent):
+class MoveEvent(ActionEvent):
 
-    def __init__(self, tile_destination: TileModel):
+    def __init__(self):
         super().__init__()
-        self.destination = tile_destination
         self.is_valid = False
         self.player = GameStateModel.instance().players_turn()
 
-    def check_valid(self):
-        num_ap = self.player.ap
-        curr_location = GameStateModel.instance().game_board.get_tile_at(self.player.x_pos, self.player.y_pos)
-        cost = 0
-        cost = self.compute_distance(curr_location, cost)
-
-    def compute_distance(self, curr_location: TileModel, cost: int):
-        pass
-
     def execute(self):
+        """Guessing what has to be done is to make everything sync up with the networking"""
         pass
