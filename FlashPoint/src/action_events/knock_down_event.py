d6f68d38e4c1c4d8e826eda853f3c2f9d1fb9e17
Abhijay Gupta <atg.abhijay@gmail.com>
2019-03-08 22:32:32 -0500

    KnockDownEvent: Small fix and question
    Using set_pos() for player; question - if player was carrying a victim, do we have to remove victim from the list of active POIs on the board?

--- FlashPoint/src/action_events/knock_down_event.py
+++ FlashPoint/src/action_events/knock_down_event.py
@@ -1,13 +1,8 @@
 from src.action_events.action_event import ActionEvent
-from src.constants.state_enums import PlayerStatusEnum, VictimStateEnum
+from src.constants.state_enums import VictimStateEnum
 from src.models.game_board.null_model import NullModel
 from src.models.game_state_model import GameStateModel
 from src.models.game_units.player_model import PlayerModel
-
-# TODO: resolve knock down. Please refer to M5 models to save some time on this:
-# Player status = KnockedDown
-# Player location = Nearest Ambulance
-#
 from src.models.game_units.victim_model import VictimModel
 
 
@@ -24,6 +19,7 @@ class KnockDownEvent(ActionEvent):
         # victim from the player and increment the
         # number of victims lost.
         if isinstance(self.player.carrying_victim, VictimModel):
+            # TODO: do we have to remove the victim from the list of active POIs of the board?
             self.player.carrying_victim.state = VictimStateEnum.LOST
             self.player.carrying_victim = NullModel()
             self.game.victims_lost += 1
@@ -37,8 +33,7 @@ class KnockDownEvent(ActionEvent):
         closest_ambulance_spots = self.game.game_board.find_closest_parking_spots("Ambulance", player_tile)
         if len(closest_ambulance_spots) == 1:
             amb_spot = closest_ambulance_spots[0]
-            self.player.row = amb_spot.row
-            self.player.column = amb_spot.column
+            self.player.set_pos(amb_spot.row, amb_spot.column)
 
         else:
             # TODO: how to handle choice for more than one closest spots?
