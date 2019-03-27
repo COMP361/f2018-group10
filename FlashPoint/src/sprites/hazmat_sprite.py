6d48f9175e04606359693e7492147a26518cef68
Seng Chiat Haw <haw1997@hotmail.com>
2019-03-27 11:02:36 -0400

    hazmat sprite

--- FlashPoint/src/sprites/hazmat_sprite.py
+++ FlashPoint/src/sprites/hazmat_sprite.py
@@ -1,7 +1,17 @@
 import pygame
 
+from src.models.game_board.tile_model import TileModel
+from src.sprites.grid_sprite import GridSprite
 
-class HazMatSprite(pygame.sprite.Sprite):
+from src.UIComponents.file_importer import FileImporter
 
-    def __init__(self):
+
+class HazmatSprite(pygame.sprite.Sprite):
+
+    def __init__(self, tile_model: TileModel, grid: GridSprite):
         super().__init__()
+        self.grid = grid
+        self.tile_model = tile_model
+        self.tile_sprite = grid.grid[tile_model.column][tile_model.row]
+        self.rect = self.tile_sprite.rect
+        self.image = FileImporter.import_image("media/all_markers/hazmat.png")
