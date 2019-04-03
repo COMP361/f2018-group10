12c723957f0e15f2238009a0212196d43cf49c34
Abhijay Gupta <atg.abhijay@gmail.com>
2019-04-03 14:11:47 -0400

    Implemented picking up and dropping Hazmats
    Move their checks to the controllers

---
+++ FlashPoint/src/action_events/turn_events/drop_hazmat_event.py
@@ -0,0 +1,35 @@
+import logging
+
+from src.action_events.turn_events.turn_event import TurnEvent
+from src.models.game_board.null_model import NullModel
+from src.models.game_state_model import GameStateModel
+from src.models.game_units.hazmat_model import HazmatModel
+
+logger = logging.getLogger("FlashPoint")
+
+class DropHazmatEvent(TurnEvent):
+
+    def __init__(self, hazmat: HazmatModel):
+        super().__init__()
+        game: GameStateModel = GameStateModel.instance()
+        self.hazmat_tile = game.game_board.get_tile_at(hazmat.row, hazmat.column)
+        self.player = game.players_turn
+
+    # TODO: Move this code to the controller.
+    def check(self):
+        """
+        If the player is not carrying a
+        hazmat, then they cannot drop it.
+
+        :return: True if player is carrying a
+                hazmat, False otherwise.
+        """
+        if isinstance(self.player.carrying_hazmat, HazmatModel):
+            return True
+
+        return False
+
+    def execute(self):
+        logger.info("Executing Drop Hazmat Event")
+        self.hazmat_tile.add_associated_model(self.player.carrying_hazmat)
+        self.player.carrying_hazmat = NullModel()
---
+++ FlashPoint/src/action_events/turn_events/pick_up_hazmat_event.py
@@ -0,0 +1,42 @@
+import logging
+
+from src.action_events.turn_events.turn_event import TurnEvent
+from src.models.game_state_model import GameStateModel
+from src.models.game_units.hazmat_model import HazmatModel
+from src.models.game_units.player_model import PlayerModel
+from src.models.game_units.victim_model import VictimModel
+
+logger = logging.getLogger("FlashPoint")
+
+class PickupHazmatEvent(TurnEvent):
+
+    def __init__(self, hazmat: HazmatModel):
+        super().__init__()
+        game: GameStateModel = GameStateModel.instance()
+        self.hazmat_tile = game.game_board.get_tile_at(hazmat.row, hazmat.column)
+        for assoc_model in self.hazmat_tile.associated_models:
+            if isinstance(assoc_model, HazmatModel):
+                self.hazmat = assoc_model
+                break
+
+        self.player: PlayerModel = game.players_turn
+
+    # TODO: Move this check code to the controller
+    def check(self) -> bool:
+        """
+        If the player is already carrying
+        another victim or a hazmat, then
+        they cannot pick up another hazmat.
+
+        :return: True if the player is carrying
+                nothing, False otherwise.
+        """
+        if isinstance(self.player.carrying_victim, VictimModel) or isinstance(self.player.carrying_hazmat, HazmatModel):
+            return False
+
+        return True
+
+    def execute(self):
+        logger.info("Executing Pickup Hazmat Event")
+        self.player.carrying_hazmat = self.hazmat
+        self.hazmat_tile.remove_associated_model(self.hazmat)
