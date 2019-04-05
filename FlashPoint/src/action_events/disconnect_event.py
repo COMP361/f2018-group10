bfe9aaa6c51816360e01cd51dabd8a78f6c45127
Seng Chiat Haw <haw1997@hotmail.com>
2019-04-05 17:13:58 -0400

    Update disconnect_event.py

--- FlashPoint/src/action_events/disconnect_event.py
+++ FlashPoint/src/action_events/disconnect_event.py
@@ -1,6 +1,10 @@
+import logging
+
 from src.models.game_state_model import GameStateModel
 from src.models.game_units.player_model import PlayerModel
 from src.action_events.action_event import ActionEvent
+
+logger = logging.getLogger("FlashPoint")
 
 
 class DisconnectEvent(ActionEvent):
@@ -9,4 +13,5 @@ class DisconnectEvent(ActionEvent):
         self._player = player
 
     def execute(self, *args, **kwargs):
+        logger.info(f"Player at {self._player.ip} has disconnected")
         GameStateModel.instance().remove_player(self._player)
