123df7e5619f280fb16540a7a0d9b9e503f696bf
Francis <francisplanning@gmail.com>
2019-04-08 19:09:44 -0400

    Grouped board setup into one event for sequential execution

--- FlashPoint/src/scenes/game_board_scene.py
+++ FlashPoint/src/scenes/game_board_scene.py
@@ -6,6 +6,7 @@ from typing import List
 
 import pygame
 
+from src.action_events.board_setup_event import BoardSetupEvent
 from src.action_events.fire_placement_event import FirePlacementEvent
 from src.action_events.set_initial_hotspot_event import SetInitialHotspotEvent
 from src.action_events.set_initial_poi_experienced_event import SetInitialPOIExperiencedEvent
@@ -147,14 +148,7 @@ class GameBoardScene(GameBoardObserver, GameStateObserver):
     def _send_game_board_initialize(self):
         """Send any game board initialization events."""
         if Networking.get_instance().is_host:
-            Networking.get_instance().send_to_all_client(FirePlacementEvent())
-
-            if self._game.rules == GameKindEnum.EXPERIENCED:
-                Networking.get_instance().send_to_all_client(SetInitialPOIExperiencedEvent())
-                Networking.get_instance().send_to_all_client(PlaceHazmatEvent())
-                Networking.get_instance().send_to_all_client(SetInitialHotspotEvent())
-            else:
-                Networking.get_instance().send_to_all_client(SetInitialPOIFamilyEvent())
+            Networking.get_instance().send_to_all_client(BoardSetupEvent())
 
         for player in self._game.players:
             player.set_initial_ap(self._game.rules)
