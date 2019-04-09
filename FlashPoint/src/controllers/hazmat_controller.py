def132978c784d5cf43be78a35635510dd6955fe
Abhijay Gupta <atg.abhijay@gmail.com>
2019-04-09 12:27:25 -0400

    Doge cannot pick up/drop a hazmat

--- FlashPoint/src/controllers/hazmat_controller.py
+++ FlashPoint/src/controllers/hazmat_controller.py
@@ -39,6 +39,10 @@ class HazmatController(Controller):
         return cls._instance
 
     def run_checks_drop(self, tile_model: TileModel):
+        # Doge cannot drop a hazmat
+        if self._current_player.role == PlayerRoleEnum.DOGE:
+            return False
+
         if isinstance(self._current_player.carrying_hazmat, HazmatModel) and self._current_player.row == tile_model.row \
                 and self._current_player.column == tile_model.column:
             return True
@@ -46,6 +50,10 @@ class HazmatController(Controller):
         return False
 
     def run_checks_pickup(self, tile_model: TileModel):
+        # Doge cannot pick up a hazmat
+        if self._current_player.role == PlayerRoleEnum.DOGE:
+            return False
+
         if isinstance(self._current_player.carrying_victim, VictimModel) or isinstance(self._current_player.carrying_hazmat, HazmatModel):
             return False
 
