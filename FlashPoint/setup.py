ff05d6cb0cc40436770ebf1fa43be5275d15990e
Francis <francisplanning@gmail.com>
2019-07-23 21:49:08 -0400

    update setup script

--- FlashPoint/setup.py
+++ FlashPoint/setup.py
@@ -1,18 +1,17 @@
-from setuptools import setup
+from setuptools import setup, find_packages
 
 """This script defines the setup for our package. It contains basic information and dependencies.
 It is called when we install our package, and creates an executable for the game."""
 
 setup(
     name='FlashPoint',
-    version='0.0.0',
-    packages=['src'],  # This must match the directory (package) containing our code.
+    version='1.0.0',
+    packages=find_packages(exclude=["*.tests"]),  # This must match the directory (package) containing our code.
     install_requires=[  # All dependencies must be in this list!!!
-        'pygame',
-        'Sphinx'
+        'pygame'
     ],
     entry_points="""
         [console_scripts]
-        FlashPoint=src.main:main
+        FlashPoint=src.main:run
     """  # Running "FlashPoint" from the command line will now start the program.
 )
